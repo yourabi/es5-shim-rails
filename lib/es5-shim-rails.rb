bef5d799b0c1b6fd0e4bb9f50f318f11accd2d8b
Tikhon Botchkarev <TikiTDO@gmail.com>
2014-07-28 07:54:17 -0400

    Update to version 4.0.1, and ensure shims are included in the asset pipeline

--- lib/es5-shim-rails.rb
+++ lib/es5-shim-rails.rb
@@ -4,6 +4,11 @@ module Es5
   module Shim
     module Rails
       class Engine < ::Rails::Engine
+        if ::Rails.version >= '3.1'
+          initializer :assets do |config|
+            ::Rails.application.config.assets.precompile += %w( es5-shim/es5-shim.js es5-shim/es5-shim-min.js es5-shim/es5-sham.js es5-shim/es5-sham-min.js )
+          end
+        end
       end
     end
   end
